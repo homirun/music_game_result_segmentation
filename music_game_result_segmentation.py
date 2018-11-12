1be5991f25d3db063b29560b75940908dc3cf29d
homirun <hatunemiku.moco@gmail.com>
2018-11-12 11:23:08 +0900

    リファクタリング

--- music_game_result_segmentation.py
+++ music_game_result_segmentation.py
@@ -10,71 +10,60 @@ from sklearn.model_selection import train_test_split
 
 
 def main():
+    img_size = 50
     dir_names = ["jubeat", "IIDX", "SDVX"]
-    img_size = 50   # イメージサイズ
 
-    imgs = []
-    labels = []  # 正解ラベル
-
-    if os.path.isfile('./music_game_score_model.h5') is False:
+    if os.path.isfile('./music_game_score_model.h5') is False:  # modelが存在していない場合
         print('create models')
-        for i, name in enumerate(dir_names):
-            path = './assets/' + name
-            files = glob.glob(path + "/*.JPG")
 
-            for file in files:
-                image = Image.open(file)
-                image = image.convert("RGB")
-                image = image.resize((img_size, img_size))
-                img_data = np.asarray(image)
-                imgs.append(img_data)
-                labels.append(i)
-
-        imgs = np.array(imgs)
-        labels = np.array(labels)
-
-        imgs = imgs.astype('float32') / 255.0
-        labels = np_utils.to_categorical(labels, len(dir_names))    # one-hot形式に変換
+        imgs, labels, names = parse_imgs(dir_names, img_size)
         imgs_train, imgs_test, labels_train, labels_test = train_test_split(imgs, labels, test_size=0.20)
 
         model = create_model(imgs_train, labels_train)
         model.save('./music_game_score_model.h5')
-        print('model save')
 
-    else:
+        print('model save')
+        print(model.evaluate(imgs_test, labels_test))
+
+    else:   # modelがすでに存在している場合
         print('load models')
+
         model = load_model('music_game_score_model.h5')
         test_files = ['jubeat_test', 'IIDX_test', 'SDVX_test']
-        names = []
 
-        for i, name in enumerate(test_files):
-            path = './assets/' + name
-            test_files = glob.glob(path + "/*.JPG")
-
-            for file in test_files:
-                names.append(file)
-                image = Image.open(file)
-                image = image.convert("RGB")
-                image = image.resize((img_size, img_size))
-                img_data = np.asarray(image)
-                imgs.append(img_data)
-                labels.append(i)
-
-        imgs_test = np.array(imgs)
-        labels_test = np.array(labels)
-
-        imgs_test = imgs_test.astype('float32') / 255.0
-        labels_test = np_utils.to_categorical(labels_test, len(dir_names))    # one-hot形式に変換
+        imgs_test, labels_test, names = parse_imgs(test_files, img_size)
 
         predicts = model.predict(imgs_test)
         for n, predict in zip(names, predicts):
             print(n, dir_names[int(np.argmax(predict))])
-        exit()
-
-    print(model.evaluate(imgs_test, labels_test))
 
 
-def create_model(imgs, labels):
+def parse_imgs(dir_names, img_size):    # 読み込んだ画像とlabelの変換
+    imgs = []
+    labels = []
+    names = []
+    for i, name in enumerate(dir_names):
+        path = './assets/' + name
+        files = glob.glob(path + "/*.JPG")
+
+        for file in files:
+            names.append(file)
+            image = Image.open(file)
+            image = image.convert("RGB")
+            image = image.resize((img_size, img_size))
+            img_data = np.asarray(image)
+            imgs.append(img_data)
+            labels.append(i)
+
+    imgs = np.array(imgs)
+    labels = np.array(labels)
+
+    imgs = imgs.astype('float32') / 255.0
+    labels = np_utils.to_categorical(labels, len(dir_names))  # one-hot形式に変換
+    return imgs, labels, names
+
+
+def create_model(imgs, labels):  # model作成
     model = Sequential()
 
     model.add(Conv2D(32, (3, 3), padding='same', input_shape=imgs.shape[1:]))
